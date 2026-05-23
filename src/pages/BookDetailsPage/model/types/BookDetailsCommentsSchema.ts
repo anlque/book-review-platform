3091a060614ed609f666c7b52193af080c3169bd
anlque <an.cr.0999@gmail.com>
2026-05-23 14:38:49 +0400

    chore(cleanup): remove extra files

--- src/pages/BookDetailsPage/model/types/BookDetailsCommentsSchema.ts
+++
@@ -1,9 +0,0 @@
-import { EntityState } from '@reduxjs/toolkit';
-import { Comment } from '@/entities/Comment';
-
-export interface BookDetailsCommentsSchema
-    extends EntityState<Comment, Comment['id']> {
-    isLoading?: boolean;
-    error?: string;
-}
-
